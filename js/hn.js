623b70e0aa53502fe15d83b1c8b178edb784831e
hnshare <68793763+hnshare@users.noreply.github.com>
2020-10-12 06:04:53 -0500

    Add files via upload

--- js/hn.js
+++ js/hn.js
@@ -1,1 +1,122 @@
-test
+ var hnmeta = {
+    lessons: {
+
+        50: {
+            title: '50. Khatima - 10 sections',
+            slides: 'Hidayatun Nahw 50 القسم الأول، الخاتمة، الفصل الأول التعريف والتنكير.pdf',
+            yt: 'https://www.youtube.com/watch?v=eA2vO2_NNDo&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=111'
+        },
+        51: {
+            title: '51. Hidayatun Nahw Lesson 51 أسماء العدد (Numbers)            ',
+            slides: 'Hidayatun Nahw 51 القسم الأول، الخاتمة، الفصل الثاني أسماء العدد.pdf',
+            yt: 'https://www.youtube.com/watch?v=_RD2Hs_KXsU&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=112',
+        },
+        52: {
+            title: '52. Hidayatun Nahw Lesson 52 المذكر والمؤنث            ',
+            slides: 'Hidayatun Nahw 52 القسم الأول، الخاتمة، الفصل الثالث، التذكير والتأنيث',
+            yt: 'https://www.youtube.com/watch?v=r6YGXXHOsh4&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=113',
+        },
+        53: {
+            title: '53. Hidayatun Nahw Lesson 53 المثنى            ',
+            slides: 'Hidayatun Nahw 53 القسم الأول، الخاتمة، الفصل الرابع، المثنی',
+            yt: 'https://www.youtube.com/watch?v=50gIy5nf-ZQ&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=114',
+        },
+        54: {
+            title: '54. Hidayatun Nahw Lesson 54 المجموع            ',
+            slides: 'Hidayatun Nahw 54 القسم الأول، الخاتمة، الفصل الخامس، المجموع',
+            yt: 'https://www.youtube.com/watch?v=YZsgxfXbuuE&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=115',
+        },
+        55: {
+            title: '55. Hidayatun Nahw Lesson 55 المصدر            ',
+            slides: 'Hidayatun Nahw 55 القسم الأول، الخاتمة، الفصل السادس، المصدر',
+            yt: 'https://www.youtube.com/watch?v=qxE0PSfwAV4&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=116',
+        },
+        56: {
+            title: '56. Hidayatun Nahw Lesson 56 اسم الفاعل            ',
+            slides: 'Hidayatun Nahw 56 القسم الأول، الخاتمة، الفصل السابع، اسم الفاعل',
+            yt: 'https://www.youtube.com/watch?v=6QdiEQgfjc8&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=117',
+        },
+        57: {
+            title: '57. Hidayatun Nahw Lesson 57 اسم المفعول            ',
+            slides: 'Hidayatun Nahw 57 القسم الأول، الخاتمة، الفصل الثامن، اسم المفعول',
+            yt: 'https://www.youtube.com/watch?v=S6CP6JIeuZs&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=118',
+        },
+        58: {
+            title: '58. Hidayatun Nahw Lesson 58 الصفة المشبهة باسم الفاعل            ',
+            slides: 'Hidayatun Nahw 58 القسم الأول، الخاتمة، الفصل التاسع، الصفة المشبهة باسم الفاعل',
+            yt: 'https://www.youtube.com/watch?v=KNttYkc3wCM&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=119',
+        },
+        59: {
+            title: '59. Hidayatun Nahw Lesson 59 اسم التفضيل            ',
+            slides: 'Hidayatun Nahw 59 القسم الأول، الخاتمة، الفصل التاسع، اسم التفضيل',
+            yt: 'https://www.youtube.com/watch?v=oHv3tUASEnM&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=120',
+        },
+
+
+
+        60: {
+            title: '60. Hidayatun Nahw Lesson 60 الفعل',
+            slides: 'Hidayatun Nahw 60 القسم الثاني،.pdf',
+            yt: 'https://www.youtube.com/watch?v=CRRi2_mNk0M&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=121',
+
+        },
+
+        61: {
+            title: '61. Hidayatun Nahw Lesson 61 الفصل الأول، أصناف إعراب الفعل',
+            yt: 'https://www.youtube.com/watch?v=UKqU4IIgxTA&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=122',
+        },
+
+        62: {
+            title: '62. Hidayatun Nahw Lesson 62 عامل المرفوع',
+            yt: 'https://www.youtube.com/watch?v=LkPDfKULCgU&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=123',
+        },
+
+        63: {
+            title: '63. Hidayatun Nahw Lesson 63 نواصب المضارع',
+            yt: 'https://www.youtube.com/watch?v=vRkrvRlUu-E&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=124',
+        },
+
+        64: {
+            yt: ['https://www.youtube.com/watch?v=65AXHO-iZOQ&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=125',
+                'https://www.youtube.com/watch?v=KLAwirXCwww&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=126',
+                'https://www.youtube.com/watch?v=4HgrlRRHSus&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=127'],
+        },
+        65: {
+            yt: 'https://www.youtube.com/watch?v=kDaQ9OX9PSM&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=128',
+        },
+        66: {
+            yt: 'https://www.youtube.com/watch?v=Hu0Uv0a1vbw&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=129',
+        },
+        67: {
+            yt: 'https://www.youtube.com/watch?v=A5Lbg9VFxHY&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=130',
+        },
+        68: {
+            yt: ['https://www.youtube.com/watch?v=YjrSaSYOt0Y&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=131',
+                'https://www.youtube.com/watch?v=JF9e-k2Etr4&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=132',
+            ],
+        },
+        69: {
+            yt: 'https://www.youtube.com/watch?v=bVNSrOfJ-5o&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=133',
+        },
+        70: {
+            yt: 'https://www.youtube.com/watch?v=Q539OmZhV80&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=134',
+        },
+
+
+        71: {
+            yt: 'https://www.youtube.com/watch?v=ojHKS5SkAAo&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=135',
+        },
+        72: {
+            yt: 'https://www.youtube.com/watch?v=7ESB8RaFjPo&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=136',
+        },
+        73: {
+            yt: ['https://www.youtube.com/watch?v=Bf1JkDzqyA4&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=137',
+            'https://www.youtube.com/watch?v=_kPAF5HTh6E&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=138',
+            'https://www.youtube.com/watch?v=4v8ZxgAVrLY&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=139',
+            'https://www.youtube.com/watch?v=X8jf9AcbnX0&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=140',
+            'https://www.youtube.com/watch?v=v8Sf-6UJM9Y&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD&index=141',
+            ]
+        },
+
+    },
+ };
