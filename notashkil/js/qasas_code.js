f9158985eeb5d0ea8a42fa015fe99e027af597f8
hnshare <68793763+hnshare@users.noreply.github.com>
2020-11-14 04:14:27 -0600

    Add files via upload

---
+++ notashkil/js/qasas_code.js
@@ -0,0 +1,65 @@
+var qasas1 = {
+    heading1: 'من كسر الأصنام    ',
+    heading2: '١ – بائع الأصنام',
+    ar: `\
+قَبْل أَيِّام كَثِيْرَة كَثِيْرة جِدّا.
+كَانَ فِيْ قَرْيَة رَجُل مَشْهُوْر مَشْهُوْر جِدّا.
+وكانَ اسْم هٰذَا الرَّجُل آزَر.
+وَكَانَ آزَر يَبِيْع الأَصْنَام.
+وَكَانَ فِيْ هٰذِهِ الْقَرْيَة بَيْت كَبِيْر جِدّا.
+وَكَانَ فِيْ هَذَا الْبَيْت أَصْنَام أَصْنَام كَثِيْرَة جدا.
+وَكَانَ النَّاس يَسْجُدُوْنَ لِهَذِهِ الْأَصْنَام.
+وَكَانَ آزَر يَسْجُدُ لِهَذِهِ الْأَصْنَام.
+وَكَانَ آزَر يَعْبُدُ هَذِهِ الْأَصْنَام.\
+`,
+
+headings_notashkil: `
+من كسر الاصنام
+
+بايع الاصنام
+`,
+
+ar_notashkil: `\
+قبل ايام كثيرة كثيرة جدا.
+كان في قرية رجل مشهور مشهور جدا.
+وكان اسم هاذا الرجل زر.
+وكان زر يبيع الاصنام.
+وكان في هاذه القرية بيت كبير جدا.
+وكان في هذا البيت اصنام اصنام كثيرة جدا.
+وكان الناس يسجدون لهذه الاصنام.
+وكان زر يسجد لهذه الاصنام.
+وكان زر يعبد هذه الاصنام.\
+`,
+
+buck: `\
+mn ksr Al>SnAm
+
+bA}E Al>SnAm
+
+qabol >ay~iAm kaviyorap kaviyorp jid~A
+kaAna fiyo qaroyap rajul ma$ohuwor ma$ohuwor jid~A
+wkAna Asom h\`*aA Alr~ajul zar
+wakaAna zar yabiyoE Al>aSonaAm
+wakaAna fiyo h\`*ihi Aloqaroyap bayot kabiyor jid~A
+wakaAna fiyo ha*aA Alobayot >aSonaAm >aSonaAm kaviyorap jdA
+wakaAna Aln~aAs yasojuduwona liha*ihi Alo>aSonaAm
+wakaAna zar yasojudu liha*ihi Alo>aSonaAm
+wakaAna zar yaEobudu ha*ihi Alo>aSonaAm\
+`,
+
+bare: `\
+mn ksr AlASnAm
+
+bAyE AlASnAm
+
+qbl AyAm kvyrp kvyrp jdA
+kAn fy qryp rjl m$hwr m$hwr jdA
+wkAn Asm hA*A Alrjl zr
+wkAn zr ybyE AlASnAm
+wkAn fy hA*h Alqryp byt kbyr jdA
+wkAn fy h*A Albyt ASnAm ASnAm kvyrp jdA
+wkAn AlnAs ysjdwn lh*h AlASnAm
+wkAn zr ysjd lh*h AlASnAm
+wkAn zr yEbd h*h AlASnAm\
+`
+};
